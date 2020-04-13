68668d95df685a14e8f11b69354d00bb3eeda10f
日涛 肖 <1761690868@qq.com>
2020-04-13 13:32:44 +0800

    准备刷新sdk

--- board/clock_config.h
+++ board/clock_config.h
@@ -1,56 +1,116 @@
-/*
- * Copyright 2017-2020 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/**
- * @file    clock_config.h
- * @brief   Board clocks header file.
- */
- 
-/* This is a template for board specific configuration created by MCUXpresso IDE Project Wizard.*/
-
 #ifndef _CLOCK_CONFIG_H_
 #define _CLOCK_CONFIG_H_
 
+#include "fsl_common.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define BOARD_XTAL0_CLK_HZ                         24000000U  /*!< Board xtal0 frequency in Hz */
+
+#define BOARD_XTAL32K_CLK_HZ                          32768U  /*!< Board xtal32k frequency in Hz */
+/*******************************************************************************
+ ************************ BOARD_InitBootClocks function ************************
+ ******************************************************************************/
+
 #if defined(__cplusplus)
 extern "C" {
-#endif /* __cplusplus */
+#endif /* __cplusplus*/
 
-/**
- * @brief 	Initialize board clocks.
+/*!
+ * @brief This function executes default configuration of clocks.
+ *
  */
 void BOARD_InitBootClocks(void);
 
 #if defined(__cplusplus)
 }
-#endif /* __cplusplus */
+#endif /* __cplusplus*/
+
+/*******************************************************************************
+ ************************** Configuration DCD_HSRUN ****************************
+ ******************************************************************************/
+/*******************************************************************************
+ * Definitions for DCD_HSRUN configuration
+ ******************************************************************************/
+#define DCD_HSRUN_CORE_CLOCK                      600000000U  /*!< Core clock frequency: 600000000Hz */
+
+/* Clock outputs (values are in Hz): */
+#define DCD_HSRUN_AHB_CLK_ROOT                        600000000UL
+#define DCD_HSRUN_CAN_CLK_ROOT                        40000000UL
+#define DCD_HSRUN_CKIL_SYNC_CLK_ROOT                  0UL
+#define DCD_HSRUN_CLKO1_CLK                           0UL
+#define DCD_HSRUN_CLKO2_CLK                           0UL
+#define DCD_HSRUN_CLK_1M                              1000000UL
+#define DCD_HSRUN_CLK_24M                             24000000UL
+#define DCD_HSRUN_CSI_CLK_ROOT                        24000000UL
+#define DCD_HSRUN_ENET1_TX_CLK                        2400000UL
+#define DCD_HSRUN_ENET_125M_CLK                       2400000UL
+#define DCD_HSRUN_ENET_25M_REF_CLK                    1200000UL
+#define DCD_HSRUN_FLEXIO1_CLK_ROOT                    30000000UL
+#define DCD_HSRUN_FLEXIO2_CLK_ROOT                    30000000UL
+#define DCD_HSRUN_FLEXSPI_CLK_ROOT                    127058823UL
+#define DCD_HSRUN_GPT1_IPG_CLK_HIGHFREQ               50000000UL
+#define DCD_HSRUN_GPT2_IPG_CLK_HIGHFREQ               50000000UL
+#define DCD_HSRUN_IPG_CLK_ROOT                        150000000UL
+#define DCD_HSRUN_LCDIF_CLK_ROOT                      67500000UL
+#define DCD_HSRUN_LPI2C_CLK_ROOT                      12000000UL
+#define DCD_HSRUN_LPSPI_CLK_ROOT                      105600000UL
+#define DCD_HSRUN_LVDS1_CLK                           1200000000UL
+#define DCD_HSRUN_MQS_MCLK                            63529411UL
+#define DCD_HSRUN_PERCLK_CLK_ROOT                     50000000UL
+#define DCD_HSRUN_PLL7_MAIN_CLK                       24000000UL
+#define DCD_HSRUN_SAI1_CLK_ROOT                       63529411UL
+#define DCD_HSRUN_SAI1_MCLK1                          63529411UL
+#define DCD_HSRUN_SAI1_MCLK2                          63529411UL
+#define DCD_HSRUN_SAI1_MCLK3                          30000000UL
+#define DCD_HSRUN_SAI2_CLK_ROOT                       63529411UL
+#define DCD_HSRUN_SAI2_MCLK1                          63529411UL
+#define DCD_HSRUN_SAI2_MCLK2                          0UL
+#define DCD_HSRUN_SAI2_MCLK3                          30000000UL
+#define DCD_HSRUN_SAI3_CLK_ROOT                       63529411UL
+#define DCD_HSRUN_SAI3_MCLK1                          63529411UL
+#define DCD_HSRUN_SAI3_MCLK2                          0UL
+#define DCD_HSRUN_SAI3_MCLK3                          30000000UL
+#define DCD_HSRUN_SEMC_CLK_ROOT                       163862068UL
+#define DCD_HSRUN_SPDIF0_CLK_ROOT                     30000000UL
+#define DCD_HSRUN_SPDIF0_EXTCLK_OUT                   0UL
+#define DCD_HSRUN_TRACE_CLK_ROOT                      99000000UL
+#define DCD_HSRUN_UART_CLK_ROOT                       80000000UL
+#define DCD_HSRUN_USBPHY1_CLK                         0UL
+#define DCD_HSRUN_USBPHY2_CLK                         0UL
+#define DCD_HSRUN_USDHC1_CLK_ROOT                     163862068UL
+#define DCD_HSRUN_USDHC2_CLK_ROOT                     163862068UL
+
+/*! @brief Arm PLL set for DCD_HSRUN configuration.
+ */
+extern const clock_arm_pll_config_t armPllConfig_DCD_HSRUN;
+/*! @brief Usb1 PLL set for DCD_HSRUN configuration.
+ */
+extern const clock_usb_pll_config_t usb1PllConfig_DCD_HSRUN;
+/*! @brief Sys PLL for DCD_HSRUN configuration.
+ */
+extern const clock_sys_pll_config_t sysPllConfig_DCD_HSRUN;
+/*! @brief Enet PLL set for DCD_HSRUN configuration.
+ */
+extern const clock_enet_pll_config_t enetPllConfig_DCD_HSRUN;
+
+/*******************************************************************************
+ * API for DCD_HSRUN configuration
+ ******************************************************************************/
+#if defined(__cplusplus)
+extern "C" {
+#endif /* __cplusplus*/
+
+/*!
+ * @brief This function executes configuration of clocks.
+ *
+ */
+void DCD_HSRUN(void);
+
+#if defined(__cplusplus)
+}
+#endif /* __cplusplus*/
 
 #endif /* _CLOCK_CONFIG_H_ */
-
 
--- source/IMRT10XX.c
+++ source/IMRT10XX.c
@@ -1,69 +1,77 @@
-/*
- * Copyright 2016-2020 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
- 
 /**
  * @file    IMRT10XX.c
  * @brief   Application entry point.
  */
-#include <stdio.h>
+#include "cm_backtrace/cm_backtrace.h"
 #include "board.h"
 #include "peripherals.h"
 #include "pin_mux.h"
 #include "clock_config.h"
 #include "MIMXRT1052.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
+#include "smartcar/sc_gpio.h"
+#include "task.h"
+#include "sc_sd.h"
+BSS_DTC uint8_t heap_heap1[64 * 1024] ALIGN(8);
+BSS_OC uint8_t heap_heap2[128 * 1024] ALIGN(8);
+BSS_SDRAM uint8_t heap_heap3[4 * 1024 * 1024] ALIGN(8);
+const HeapRegion_t xHeapRegions[] =
+        {
+                {&heap_heap1[0], sizeof(heap_heap1)},
+                {&heap_heap2[0], sizeof(heap_heap2)},
+                {&heap_heap3[0], sizeof(heap_heap3)},
+                {NULL,           0} /* Terminates the array. */
+        };
 
-/* TODO: insert other definitions and declarations here. */
+gpio_t wakeUp = {WAKEUP_GPIO, WAKEUP_PIN, kGPIO_DigitalInput};
+gpio_t led = {LED1_GPIO, LED1_PIN, kGPIO_DigitalOutput};
+TaskHandle_t LED_task_handle;
+TaskHandle_t start_task_handle;
 
-/*
- * @brief   Application entry point.
- */
+void LED_task(void *pvData) {
+    while (1) {
+        GPIO_Set(&led);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        GPIO_Clear(&led);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+//extern void adc(void *pv);
+//extern void cpp(void *pv);
+//extern void flash_lfs(void *pv);
+//extern void flash(void *pv);
+//extern void pit(void *pv);
+
+TaskHandle_t handle;
+
+void start_task(void *pvData) {
+    PRINTF("press wakeup key to start\r\n");
+    while (GPIO_Read(&wakeUp)) {
+        vTaskDelay(10);
+    }
+
+    SD_MscInit();
+
+//    xTaskCreate(adc,"adc example",512,NULL,1,&handle);
+//    xTaskCreate(cpp,"cpp example",512,NULL,1,&handle);
+//    xTaskCreate(pit,"pit example",512,NULL,1,&handle);
+    vTaskDelete(NULL);
+}
+
 int main(void) {
-  	/* Init board hardware. */
+    BOARD_ConfigVectors();
+    BOARD_ConfigMPU();
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
-  	/* Init FSL debug console. */
-	BOARD_InitDebugConsole();
-
-    PRINTF("Hello World\n");
-
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
+    BOARD_InitDebugConsole();
+    PRINTF("\r\n*********\n");
+    cm_backtrace_init("imxrt.axf", "0.1.0", "0.0.1");
+    vPortDefineHeapRegions(xHeapRegions);
+    xTaskCreate(LED_task, "LED_task", 64, NULL, 3, &LED_task_handle);
+    //这个任务除了测试flash之外还会记录一共启动了多少次
+//    xTaskCreate(flash_lfs,"flash_lfs example",512,NULL,3,&handle);
+    xTaskCreate(start_task, "start_task", 1028, NULL, 2, &start_task_handle);
+    vTaskStartScheduler();
+    return 0;
 }
